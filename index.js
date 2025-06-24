eed739d27c3e6157f3ac53cb840520b15e0ab2c0
Duaxir <duaxir.contact@gmail.com>
2025-06-24 18:21:50 +0200

    Files of index.js and routes/users.js have been modified.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const usersRouter = require('./routes/users');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/v1', usersRouter);
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
